83d1242991c13dc633aaa91995383ab9c267dd68
IkerDelgado <a23ikedelgra@inspedralbes.cat>
2024-12-16 09:07:43 +0100

    final

--- front/vuetify-project/src/pages/index.vue
+++ front/vuetify-project/src/pages/index.vue
@@ -9,7 +9,7 @@
 import CarrilesCarrera from '@/components/CarrilesCarrera.vue';
 import CarrilJugador from '@/components/CarrilJugador.vue';
 import dado from '@/components/dado.vue';
-import Estadisticas from '@/components/ComponenteEstadisticas.vue';
-import ComponenteEstadisticas from '@/components/ComponenteEstadisticas.vue';
+import Estadisticas from '@/components/Estadisticas.vue';
+import ComponenteEstadisticas from '@/components/Estadisticas.vue';
 
 </script>
